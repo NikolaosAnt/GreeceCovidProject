104c5ecaa55225fa536ed8f04b9a123f3902774d
NikolaosAnt <106773365+NikolaosAnt@users.noreply.github.com>
2022-06-15 19:15:50 +0300

    Update CovidData for Greece.sql

--- CovidData for Greece.sql
+++ CovidData for Greece.sql
@@ -1,3 +1,4 @@
+--Testing to check if tables are successfully imported
 Select *
 From dbo.CovidDeaths
 where continent is not null
@@ -5,31 +6,31 @@ Order by 3,4
 
 Select *
 From dbo.Vacci
+where continent is not null
 Order by 3,4
 
---Data to be used
+--Decide with which data to work with
 
 Select Location, date, total_cases, new_cases, total_deaths, population
 From dbo.CovidDeaths
 where continent is not null
 Order by 1,2
 
---DeathPercent in Greece= Total Deaths/Total Cases
+--Considering Greece as the location, I calculate the DeathPercent (Total Deaths/Total Cases)
 
 Select Location, date, total_cases, total_deaths, population, (total_deaths/total_cases)*100 as DeathPercent
 From dbo.CovidDeaths
 Where location='Greece' and continent is not null
 Order by 1,2
 
---Total Cases vs Population
---Meaning the % of total population that got Covid
+--Greece's Total Cases vs Population
 
 Select Location, date, total_cases, population, (total_cases/population)*100 as PopulationCovidPercent
 From dbo.CovidDeaths
 Where location='Greece' and continent is not null
 Order by 1,2
 
---Countries with highest infection Rate to Population
+--Globally: The percent of each country's Covid infections
 
 Select Location, population, MAX(total_cases) as HighestInfections, MAX(total_cases/population)*100 As PopulationCovidPercent
 From dbo.CovidDeaths
@@ -37,36 +38,25 @@ where continent is not null
 Group by population, location
 Order by PopulationCovidPercent Desc
 
---Countries with Most Deaths to Population per Location
+--CovidDeaths per continent
 
-Select location, Max(cast(total_deaths as int)) as MostDeaths
-From dbo.CovidDeaths
-where continent is null
-Group by location
-Order by MostDeaths Desc 
-
----Same search for Continents
- 
-
---Continents with the highest death rate (total_deaths/population)
 Select continent, Max(cast (total_deaths as int)) as MostDeaths
 From dbo.CovidDeaths
 where continent is not null
 group by continent
 order by MostDeaths desc
 
----Broader Picture
---DeathPercent daily
+--Global DeathPercent per day
 
 Select date, SUM(new_cases) as total_cases, SUM(Cast(new_deaths as int)) as total_deaths, SUM(Cast(new_deaths as int))/SUM(new_cases)*100 as DeathPercent
 From dbo.CovidDeaths
 Where continent is not null
 group by date
-order by 1,2
+order by 2,3
 
 
---JOINING 
---TOTAL POP VS VACCI IN GREECE
+--Joining the two tables (Covid.Deaths vs Covid.Vacc 
+--RunningSum of vaccinations in Greece per day
 
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(cast(vac.new_vaccinations as int)) OVER (Partition by dea.location Order by dea.location,dea.date) as VaccinationRunningSum
@@ -78,9 +68,13 @@ Join dbo.Vacci vac
 Where dea.continent is not null and dea.location='Greece'
 Order by 2,3 
 
------Create a Common Table Expression---------
---------This is how to define the temporary VIEW's name--------
-With Population_vs_Vaccinations(Continent,location,date,population, new_vaccinations, VaccinationRunningSum)
+------------------------------------------------------------------------------------------------
+/*Create a Common Table Expression to extract VaccinationRunningSum 
+(writing a SELECT query which will give you a result to use within another query)
+in order to find VaccinationRunningPercent
+First, we consider only Greece*/
+
+With Population_vs_Vaccinations 
 as
 (Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(cast(vac.new_vaccinations as int)) OVER (Partition by dea.location Order by dea.location,dea.date) as VaccinationRunningSum
@@ -96,30 +90,40 @@ Where dea.continent is not null and dea.location='Greece'
 Select *, (VaccinationRunningSum/population)*100 as VaccinationRunningSumPercent
 From Population_vs_Vaccinations
 
---temp table--
-Create table PercentageofPopulationVaccinated
+
+
+--Create a temp table for PercentageofGreeksVaccinated
+--Also using a Drop table statement, in case we need to alter anything in the table--
+
+DROP TABLE IF EXISTS PercentageofGreeksVaccinated
+CREATE TABLE PercentageofGreeksVaccinated
 (
-continent nvarchar(255)
-location nvarchar(255)
+continent nvarchar(255),
+location nvarchar(255),
 Date datetime,
-population int,
-new_vaccinations int,
+population numeric(18,0),
+new_vaccinations numeric(18,0),
+VaccinationRunningSum numeric(18,0)
+)
 
-
-Insert into
+Insert into PercentageofGreeksVaccinated
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
-, SUM(cast(vac.new_vaccinations as int)) OVER (Partition by dea.location Order by dea.location,dea.date) as VaccinationRunningSum
+, SUM(cast(vac.new_vaccinations as bigint)) OVER (Partition by dea.location Order by dea.location,dea.date) as VaccinationRunningSum
 --, (VaccinationRunningSum/population)*100--(cannot use a column that is just created)--
 From dbo.CovidDeaths dea
 Join dbo.Vacci vac
 	On dea.location=vac.location
 	and dea.date=vac.date
 Where dea.continent is not null and dea.location='Greece'
---Order by 2,3
+Order by 2,3
 
+Select *, (VaccinationRunningSum/population)*100 as VaccinationRunningSumPercent
+From PercentageofGreeksVaccinated
 
 ----Create view to store data for later vizualization---
 --Location stays as Greece!--
+--Tableau Public does not allow users to import data from databases, so I cannot import the dataset to Tableau :(--
+
 Create View VaccinationRunningSum as 
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(cast(vac.new_vaccinations as int)) OVER (Partition by dea.location Order by dea.location,dea.date) as VaccinationRunningSum
