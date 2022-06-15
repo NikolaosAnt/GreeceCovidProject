c06d79f38a1282c508cf4118f221830dfd8a0a48
NikolaosAnt <106773365+NikolaosAnt@users.noreply.github.com>
2022-06-15 21:38:59 +0300

    Update CovidData for Greece.sql

--- CovidData for Greece.sql
+++ CovidData for Greece.sql
@@ -1,58 +1,72 @@
 --Testing to check if tables are successfully imported
 Select *
-From dbo.CovidDeaths
+From PortfolioProject1.dbo.CovidDeaths
 where continent is not null
 Order by 3,4
 
 Select *
-From dbo.Vacci
+From PortfolioProject1.dbo.Vacci
 where continent is not null
 Order by 3,4
 
 --Decide with which data to work with
 
 Select Location, date, total_cases, new_cases, total_deaths, population
-From dbo.CovidDeaths
+From PortfolioProject1.dbo.CovidDeaths
 where continent is not null
 Order by 1,2
 
 --Considering Greece as the location, I calculate the DeathPercent (Total Deaths/Total Cases)
 
 Select Location, date, total_cases, total_deaths, population, (total_deaths/total_cases)*100 as DeathPercent
-From dbo.CovidDeaths
+From PortfolioProject1.dbo.CovidDeaths
 Where location='Greece' and continent is not null
 Order by 1,2
 
 --Greece's Total Cases vs Population
 
 Select Location, date, total_cases, population, (total_cases/population)*100 as PopulationCovidPercent
-From dbo.CovidDeaths
+From PortfolioProject1.dbo.CovidDeaths
 Where location='Greece' and continent is not null
 Order by 1,2
+
+--Greece Total_cases, Total_deaths & DeathPercent
+Select SUM(new_cases) as total_cases, SUM(Cast(new_deaths as int)) as total_deaths, SUM(Cast(new_deaths as int))/SUM(new_cases)*100 as DeathPercent
+From PortfolioProject1.dbo.CovidDeaths
+Where continent is not null and location='Greece'
+--group by date
+order by 1,2
 
 --Globally: The percent of each country's Covid infections
 
 Select Location, population, MAX(total_cases) as HighestInfections, MAX(total_cases/population)*100 As PopulationCovidPercent
-From dbo.CovidDeaths
+From PortfolioProject1.dbo.CovidDeaths
 where continent is not null
 Group by population, location
 Order by PopulationCovidPercent Desc
 
---CovidDeaths per continent
+--DeathSum per continent
 
-Select continent, Max(cast (total_deaths as int)) as MostDeaths
-From dbo.CovidDeaths
-where continent is not null
-group by continent
-order by MostDeaths desc
+Select location, Sum(cast(new_deaths as int)) as DeathSum
+From PortfolioProject1.dbo.CovidDeaths
+Where continent is null and location not in ('world','International','upper middle income','high income','lower middle income','low income','European Union')
+Group by location
+Order by DeathSum desc
 
 --Global DeathPercent per day
 
 Select date, SUM(new_cases) as total_cases, SUM(Cast(new_deaths as int)) as total_deaths, SUM(Cast(new_deaths as int))/SUM(new_cases)*100 as DeathPercent
-From dbo.CovidDeaths
+From PortfolioProject1.dbo.CovidDeaths
 Where continent is not null
 group by date
 order by 2,3
+
+----DeathSum in rest of the world
+Select location,Sum(cast(new_deaths as int)) as DeathSum
+From PortfolioProject1.dbo.CovidDeaths
+Where continent is not null and location not like 'Greece'
+Group by location
+Order by DeathSum desc
 
 
 --Joining the two tables (Covid.Deaths vs Covid.Vacc 
@@ -61,8 +75,8 @@ order by 2,3
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(cast(vac.new_vaccinations as int)) OVER (Partition by dea.location Order by dea.location,dea.date) as VaccinationRunningSum
 --, (VaccinationRunningSum/population)*100
-From dbo.CovidDeaths dea
-Join dbo.Vacci vac
+From PortfolioProject1.dbo.CovidDeaths dea
+Join PortfolioProject1.dbo.Vacci vac
 	On dea.location=vac.location
 	and dea.date=vac.date
 Where dea.continent is not null and dea.location='Greece'
@@ -79,8 +93,8 @@ as
 (Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(cast(vac.new_vaccinations as int)) OVER (Partition by dea.location Order by dea.location,dea.date) as VaccinationRunningSum
 --, (VaccinationRunningSum/population)*100--(cannot use a column that is just created)--
-From dbo.CovidDeaths dea
-Join dbo.Vacci vac
+From PortfolioProject1.dbo.CovidDeaths dea
+Join PortfolioProject1.dbo.Vacci vac
 	On dea.location=vac.location
 	and dea.date=vac.date
 Where dea.continent is not null and dea.location='Greece'
@@ -95,8 +109,8 @@ From Population_vs_Vaccinations
 --Create a temp table for PercentageofGreeksVaccinated
 --Also using a Drop table statement, in case we need to alter anything in the table--
 
-DROP TABLE IF EXISTS PercentageofGreeksVaccinated
-CREATE TABLE PercentageofGreeksVaccinated
+DROP TABLE IF EXISTS PercentofGreeksVaccinated
+CREATE TABLE PercentofGreeksVaccinated
 (
 continent nvarchar(255),
 location nvarchar(255),
@@ -106,19 +120,19 @@ new_vaccinations numeric(18,0),
 VaccinationRunningSum numeric(18,0)
 )
 
-Insert into PercentageofGreeksVaccinated
+Insert into PercentofGreeksVaccinated
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(cast(vac.new_vaccinations as bigint)) OVER (Partition by dea.location Order by dea.location,dea.date) as VaccinationRunningSum
 --, (VaccinationRunningSum/population)*100--(cannot use a column that is just created)--
-From dbo.CovidDeaths dea
-Join dbo.Vacci vac
+From PortfolioProject1.dbo.CovidDeaths dea
+Join PortfolioProject1.dbo.Vacci vac
 	On dea.location=vac.location
 	and dea.date=vac.date
 Where dea.continent is not null and dea.location='Greece'
 Order by 2,3
 
 Select *, (VaccinationRunningSum/population)*100 as VaccinationRunningSumPercent
-From PercentageofGreeksVaccinated
+From PercentofGreeksVaccinated
 
 ----Create view to store data for later vizualization---
 --Location stays as Greece!--
@@ -128,8 +142,8 @@ Create View VaccinationRunningSum as
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(cast(vac.new_vaccinations as int)) OVER (Partition by dea.location Order by dea.location,dea.date) as VaccinationRunningSum
 --, (VaccinationRunningSum/population)*100--(cannot use a column that is just created)--
-From dbo.CovidDeaths dea
-Join dbo.Vacci vac
+From PortfolioProject1.dbo.CovidDeaths dea
+Join PortfolioProject1.dbo.Vacci vac
 	On dea.location=vac.location
 	and dea.date=vac.date
 Where dea.continent is not null and dea.location='Greece'
